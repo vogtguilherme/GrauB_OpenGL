2a433feb229448b0142d14b8fd89ce787f2f68be
Guilherme Vogt <vogt.guilherme@gmail.com>
2018-11-27 18:03:43 -0200

    Funcionalidade da troca de "cenas"
    Problema com métodos chamados dentro das funções da opengl
    
    #"non-standard syntax; use '&' to create a pointer to member	"

--- GrauB_OpenGL/Scene.cpp
+++ GrauB_OpenGL/Scene.cpp
@@ -2,31 +2,279 @@
 
 void Scene::Update()
 {
+	Render();
 
+	Playing();
 }
 
-void Scene::GetKeyboardInput()
+void Scene::GetKeyboardInput(unsigned char key, int x, int y)
 {
+	switch (key)
+	{
+	case 'a':
+		if (jogador.x > -39) jogador.Movimenta(-1);
+		break;
+	case 'd':
+		if (jogador.x < 39) jogador.Movimenta(1);
+		break;
+	}
 
+	glutPostRedisplay();
 }
 
-void Scene::GetMouseInput()
+void Scene::GetMouseInput(int button, int state, int x, int y)
 {
+	if (button == GLUT_LEFT_BUTTON)
+		if (state == GLUT_DOWN)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				if (bullets[i].usada == false)
+				{
+					bullets[i].CriaBullet(jogador.x, jogador.y, jogador.z - 10);
+					bullets[i].usada = true;
 
+					std::cout << "Taca Fogo";
+
+					break;
+				}
+			}
+		}
+}
+
+void Scene::SetWindowSize(GLsizei w, GLsizei h)
+{
+	// Para previnir uma divisão por zero
+	if (h == 0) h = 1;
+
+	// Especifica o tamanho da viewport
+	glViewport(0, 0, w, h);
+
+	// Calcula a correção de aspecto
+	fAspect = (GLfloat)w / (GLfloat)h;
+
+	SpecifyViewParameters();
+}
+
+void Scene::SpecifyViewParameters()
+{
+	// Especifica sistema de coordenadas de projeção
+	glMatrixMode(GL_PROJECTION);
+	// Inicializa sistema de coordenadas de projeção
+	glLoadIdentity();
+
+	// Especifica a projeção perspectiva
+	gluPerspective(angle, fAspect, 0.4, 3000);
+
+	// Especifica sistema de coordenadas do modelo
+	glMatrixMode(GL_MODELVIEW);
+	// Inicializa sistema de coordenadas do modelo
+
+	// Especifica posição do observador e do alvo
+	gluLookAt(0, 40, 80, 0, 25, 0, 0, 10, 0);
 }
 
 void Scene::Render()
 {
+	// Limpa a janela e o depth buffer
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//glColor3f(0.0f, 0.0f, 1.0f);
+
+	// Desenha uma esfera sólida
+	//glutSolidSphere(50.0f, 100, 100);
+
+	Pecas.criaMar();
+	Pecas.CriaParedes();
+
+	/*
+	glRotatef(cubeAngle, cubeX, cubeY, cubeZ);
+	glTranslatef(moveX, moveY, moveZ);
+	glutSolidCube(50);
+	*/
+
+	for (int i = 0; i < 5; i++)
+	{
+		IA[i].DesenhaInimigo();
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		bullets[i].DesenhaBullet();
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		fuel[i].DesenhaFuel();
+
+	}
+
+	jogador.Desenhajogador();
+
+	/*EscreveVidas();
+	EscreveKills();
+	EscreveFuel();*/
+
+	glutSwapBuffers();
 
 }
 
 void Scene::Start()
 {
+	angle = 35;
+	cubeAngle = 0;
+	cubeX = 0;
+	cubeY = 1.0;
+	cubeZ = 0;
+	moveX = 0;
+	moveY = 0;
+	moveZ = 0;
 
+	jogador.CriaPlayer(0, 5, 0);
+	jogador.vidas = 3;
+	jogador.acertos = 0;
+	jogador.combustivel = 100;
+
+	for (int i = 0; i < 3; i++)
+	{
+		fuel[i].CriaFuel(rand() % 70 - 35, 5.0f, -1100 * (i + 1));
+		fuel[i].speed = 2.0f;
+		fuel[i].combustivel = 50.0f;
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		IA[i].CriaInimigo(rand() % 80 - 40, 5.0f, -1000 * (i + 1));
+		IA[i].speed = 1.0f;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		bullets[i].CriaBullet(0, jogador.y, 1000);
+		bullets[i].usada = false;
+		bullets[i].speed = 5;
+	}
 }
 
 void Scene::MainMenu()
 {
+}
+
+void Scene::Playing()
+{
+	jogador.combustivel -= (100.0f / 30.0f) / 60.0f;
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (IA[i].z < 100) IA[i].Movimento(0, IA[i].speed * speed);
+		else
+		{
+			IA[i].CriaInimigo(rand() % 80 - 40, IA[i].y, -4000);
+			if (IA[i].speed < 20) IA[i].speed += IA[i].speed * 0.1f;
+		}
+
+		if (jogador.x - 1.5f < IA[i].x + 10 && jogador.x + 1.5f > IA[i].x - 10)
+		{
+			if (jogador.y - 1.5f < IA[i].y + 10 && jogador.y + 1.5f > IA[i].y - 10)
+			{
+				if (jogador.z - 1.5f < IA[i].z + 10 && jogador.z + 1.5f > IA[i].z - 10)
+				{
+					//std::cout << "BAteu Carai z";
+
+					IA[i].CriaInimigo(50000, IA[i].y, IA[i].z);
+
+					jogador.vidas--;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		fuel[i].Movimenta(0, fuel[i].speed);
+
+		if (fuel[i].z > 100) fuel[i].CriaFuel(rand() % 70 - 35, fuel[i].y, -4000);
+
+		if (jogador.x - 1.5f < fuel[i].x + 10 && jogador.x + 1.5f > fuel[i].x - 10)
+		{
+			if (jogador.y - 1.5f < fuel[i].y + 10 && jogador.y + 1.5f > fuel[i].y - 10)
+			{
+				if (jogador.z - 1.5f < fuel[i].z + 10 && jogador.z + 1.5f > fuel[i].z - 10)
+				{
+					if (jogador.combustivel + fuel[i].combustivel < 100) jogador.combustivel += fuel[i].combustivel;
+					else jogador.combustivel = 100;
+
+					fuel[i].CriaFuel(50000, fuel[i].y, fuel[i].x);
+				}
+			}
+		}
+	}
+
+	for (int j = 0; j < 10; j++)
+	{
+		if (bullets[j].usada == true)
+		{
+			bullets[j].Movimento(0, -bullets[j].speed);
+
+			if (bullets[j].z < -2000)
+			{
+				bullets[j].CriaBullet(0, jogador.y, 1000);
+				bullets[j].usada = false;
+			}
+
+			for (int i = 0; i < 5; i++)
+			{
+				if (bullets[j].x - 0.5f < IA[i].x + 10 && bullets[j].x + 0.5f > IA[i].x - 10)
+				{
+					if (bullets[j].y - 0.5f < IA[i].y + 10 && bullets[j].y + 0.5f > IA[i].y - 10)
+					{
+						if (bullets[j].z - 0.5f < IA[i].z + 10 && bullets[j].z + 0.5f > IA[i].z - 10)
+						{
+							IA[i].CriaInimigo(50000, IA[i].y, IA[i].z);
+
+							bullets[j].CriaBullet(0, jogador.y, 1000);
+							bullets[j].usada = false;
+
+							jogador.acertos++;
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+	/*
+	if (lado == true)
+	{
+	if (moveX < 0)
+	{
+	//IA.Movimento(moveX - speed, 0.0f, 0.0f);
+	Pecas.Mmovimenta(moveX - speed);
+	}
+	if (moveX >= 10)
+	{
+	lado = false;
+	}
+	moveX++;
+	std::cout << "valor em x: " << moveX << std::endl;
+
+	}
+	if (lado == false) {
+	if (moveX < 50)
+	{
+	// IA.Movimento(moveX - speed, 0.0, 0.0);
+	Pecas.Mmovimenta(moveX - speed);
+	}
+
+	if (moveX <= -10)
+	{
+	lado = true;
+	}
+	moveX--;
+	std::cout << "valor em x: " << moveX << std::endl;
+
+	}*/
 }
 
 void Scene::Pause()
--- GrauB_OpenGL/Scene.h
+++ GrauB_OpenGL/Scene.h
@@ -1,20 +1,51 @@
 #pragma once
+
+#include<gl\freeglut.h>
+#include<iostream>
+
+#include "Bullet.h"
+#include "Jogador.h"
+#include "Inimigo.h"
+#include "Objetos.h"
+#include "Fuel.h"
+
 class Scene
 {
 public:
 
+	GLfloat fAspect, angle;
+
+	GLfloat cubeAngle, cubeX, cubeY, cubeZ, moveX, moveY, moveZ;
+
+	const int FPS = 60;
+
 	void Render();
 	void Start();
 	void Update();
-	void GetKeyboardInput();
-	void GetMouseInput();
+	void GetKeyboardInput(unsigned char key, int x, int y);
+	void GetMouseInput(int button, int state, int x, int y);
+	void SetWindowSize(GLsizei w, GLsizei h);
+	void SpecifyViewParameters();
 
 	void MainMenu();
+	void Playing();
 	void Pause();
 	void GameOver();
 	void Help();
 
 	Scene();
 	~Scene();
+
+private:
+
+	Bullet bullets[1];
+	Jogador jogador;
+	Inimigo IA[5];
+	Fuel fuel[3];
+	Objetos Pecas;	
+
+	bool lado = true;
+
+	float speed = 5.0f;
 };
 
