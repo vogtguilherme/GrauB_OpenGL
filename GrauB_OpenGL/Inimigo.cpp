9dd1ee3b38be5bd85811b01adc40b54c3dd8db4c
miltinhodabonja <miltongustavo3@gmail.com>
2018-11-28 04:08:33 -0200

    criacao dos inimigos 3d
    ta sendo criado mas nao ta aparecendo

--- GrauB_OpenGL/Inimigo.cpp
+++ GrauB_OpenGL/Inimigo.cpp
@@ -15,41 +15,135 @@ void Inimigo::DesenhaInimigo()
 	glutSolidCube(20);
 	glPopMatrix();*/
 
+	
+	//base
 	glPushMatrix();
-	glTranslatef(x, y, z);
-	glRotatef(45, x, y, z);
 	glScalef(8, 8, 8);
-	//helice
-	glColor3f(1.0, 1.0, 0.0);
+	glTranslatef(x, y, z);
+	glColor3f(1.0, 0.0, 0.0);
 	glBegin(GL_QUADS);
-	glVertex3f(0.1f, 0.5f, 0.5);
-	glVertex3f(0.1f, 0.7f, 0.5);
-	glVertex3f(1.0f, 0.7f, 0.5);
-	glVertex3f(1.0f, 0.5f, 0.5);
-	glEnd();
-
-	//tronco
-	glColor3f(1.0, 1.0, 0.0);
-	glBegin(GL_QUADS);
-	glVertex3f(0.5f, 0.3f, 0.5);
-	glVertex3f(0.5f, 0.5f, 0.5);
-	glVertex3f(0.6f, 0.5f, 0.5);
-	glVertex3f(0.6f, 0.3f, 0.5);
-	glEnd();
-
-	//corpo
-	glColor3f(1.0, 1.0, 0.0);
-	glBegin(GL_QUADS);
-	glVertex3f(0.3f, 0.1f, 0.5);
-	glVertex3f(0.3f, 0.3f, 0.5);
-	glVertex3f(1.0f, 0.3f, 0.5);
-	glVertex3f(1.0f, 0.1f, 0.5);
-	glVertex3f(0.8f, 0.1f, 0.5);
-	glVertex3f(0.7f, 0.0f, 0.5);
-	glVertex3f(0.4f, 0.0f, 0.5);
-	glVertex3f(0.3f, 0.1f, 0.5);
+	glVertex3f(0.1f, 0.3f, 0.0);
+	glVertex3f(0.6f, 0.3f, 0.0);
+	glVertex3f(0.5f, 0.1f, 0.0);
+	glVertex3f(0.2f, 0.1f, 0.0);
 	glEnd();
 	glPopMatrix();
+
+
+	glPushMatrix();
+	glColor3f(1.0, 0.0, 0.0);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glTranslatef(0.0, 0.0, 0.1);
+	glBegin(GL_QUADS);
+	glVertex3f(0.1f, 0.3f, 0.0);
+	glVertex3f(0.6f, 0.3f, 0.0);
+	glVertex3f(0.5f, 0.1f, 0.0);
+	glVertex3f(0.2f, 0.1f, 0.0);
+	glEnd();
+	glPopMatrix();
+	//fim base
+
+	//tronco
+	glPushMatrix();
+	glColor3f(0.74, 0.56, 0.56);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glBegin(GL_QUADS);
+	glVertex3f(0.3f, 0.3f, 0.0);
+	glVertex3f(0.3f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.3f, 0.0);
+	glEnd();
+	glPopMatrix();
+
+
+	glPushMatrix();
+	glColor3f(0.74, 0.56, 0.56);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glTranslatef(0.0, 0.0, 0.1);
+	glBegin(GL_QUADS);
+	glVertex3f(0.3f, 0.3f, 0.0);
+	glVertex3f(0.3f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.3f, 0.0);
+	glEnd();
+	glPopMatrix();
+
+
+	glPushMatrix();
+	glColor3f(0.74, 0.56, 0.56);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glTranslatef(0.4, 0.0, 0.4);
+	glRotatef(90, 0.0, 1.0, 0.0);
+	glBegin(GL_QUADS);
+	glVertex3f(0.3f, 0.3f, 0.0);
+	glVertex3f(0.3f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.3f, 0.0);
+	glEnd();
+	glPopMatrix();
+
+	glPushMatrix();
+	glColor3f(0.74, 0.56, 0.56);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glTranslatef(0.3, 0.0, 0.4);
+	glRotatef(90, 0.0, 1.0, 0.0);
+	glBegin(GL_QUADS);
+	glVertex3f(0.3f, 0.3f, 0.0);
+	glVertex3f(0.3f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.3f, 0.0);
+	glEnd();
+	glPopMatrix();
+
+	glPushMatrix();
+	glColor3f(0.74, 0.56, 0.56);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glTranslatef(-0.2, 0.1, -0.2);
+	glRotatef(90, 1.0, 0.0, 0.0);
+	glBegin(GL_QUADS);
+	glVertex3f(0.7f, 0.3f, 0.0);
+	glVertex3f(0.7f, 0.2f, 0.0);
+	glVertex3f(0.4f, 0.2f, 0.0);
+	glVertex3f(0.4f, 0.3f, 0.0);
+	glEnd();
+	glPopMatrix();
+
+
+	glPushMatrix();
+	glColor3f(0.74, 0.56, 0.56);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glTranslatef(-0.3, 0.3, -0.2);
+	glRotatef(90, 1.0, 0.0, 0.0);
+	glBegin(GL_QUADS);
+	glVertex3f(0.9f, 0.3f, 0.0);
+	glVertex3f(0.9f, 0.2f, 0.0);
+	glVertex3f(0.4f, 0.2f, 0.0);
+	glVertex3f(0.4f, 0.3f, 0.0);
+	glEnd();
+	glPopMatrix();
+
+	//fim tronco
+
+	//velas
+	glPushMatrix();
+	glColor3f(0.75, 1.0, 0.0);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glBegin(GL_TRIANGLES);
+	glVertex3f(0.4f, 0.3f, 0.0);
+	glVertex3f(0.4f, 0.5f, 0.0);
+	glVertex3f(0.6f, 0.3f, 0.0);
+	glEnd();
+	glPopMatrix();
+
+	
 }
 
 void Inimigo::Movimento(float _x, float _z)
@@ -64,41 +158,133 @@ void Inimigo::Movimento(float _x, float _z)
 	glutSolidCube(20);
 	glPopMatrix();*/
 
+	///base
 	glPushMatrix();
-	glTranslatef(x, y, z);
-	glRotatef(45, x, y, z);
 	glScalef(8, 8, 8);
-	//helice
-	glColor3f(1.0, 1.0, 0.0);
+	glTranslatef(x, y, z);
+	glColor3f(1.0, 0.0, 0.0);
 	glBegin(GL_QUADS);
-	glVertex3f(0.1f, 0.5f, 0.5);
-	glVertex3f(0.1f, 0.7f, 0.5);
-	glVertex3f(1.0f, 0.7f, 0.5);
-	glVertex3f(1.0f, 0.5f, 0.5);
-	glEnd();
-
-	//tronco
-	glColor3f(1.0, 1.0, 0.0);
-	glBegin(GL_QUADS);
-	glVertex3f(0.5f, 0.3f, 0.5);
-	glVertex3f(0.5f, 0.5f, 0.5);
-	glVertex3f(0.6f, 0.5f, 0.5);
-	glVertex3f(0.6f, 0.3f, 0.5);
-	glEnd();
-
-	//corpo
-	glColor3f(1.0, 1.0, 0.0);
-	glBegin(GL_QUADS);
-	glVertex3f(0.3f, 0.1f, 0.5);
-	glVertex3f(0.3f, 0.3f, 0.5);
-	glVertex3f(1.0f, 0.3f, 0.5);
-	glVertex3f(1.0f, 0.1f, 0.5);
-	glVertex3f(0.8f, 0.1f, 0.5);
-	glVertex3f(0.7f, 0.0f, 0.5);
-	glVertex3f(0.4f, 0.0f, 0.5);
-	glVertex3f(0.3f, 0.1f, 0.5);
+	glVertex3f(0.1f, 0.3f, 0.0);
+	glVertex3f(0.6f, 0.3f, 0.0);
+	glVertex3f(0.5f, 0.1f, 0.0);
+	glVertex3f(0.2f, 0.1f, 0.0);
 	glEnd();
 	glPopMatrix();
+
+
+	glPushMatrix();
+	glColor3f(1.0, 0.0, 0.0);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glTranslatef(0.0, 0.0, 0.1);
+	glBegin(GL_QUADS);
+	glVertex3f(0.1f, 0.3f, 0.0);
+	glVertex3f(0.6f, 0.3f, 0.0);
+	glVertex3f(0.5f, 0.1f, 0.0);
+	glVertex3f(0.2f, 0.1f, 0.0);
+	glEnd();
+	glPopMatrix();
+	//fim base
+
+	//tronco
+	glPushMatrix();
+	glColor3f(0.74, 0.56, 0.56);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glBegin(GL_QUADS);
+	glVertex3f(0.3f, 0.3f, 0.0);
+	glVertex3f(0.3f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.3f, 0.0);
+	glEnd();
+	glPopMatrix();
+
+
+	glPushMatrix();
+	glColor3f(0.74, 0.56, 0.56);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glTranslatef(0.0, 0.0, 0.1);
+	glBegin(GL_QUADS);
+	glVertex3f(0.3f, 0.3f, 0.0);
+	glVertex3f(0.3f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.3f, 0.0);
+	glEnd();
+	glPopMatrix();
+
+
+	glPushMatrix();
+	glColor3f(0.74, 0.56, 0.56);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glTranslatef(0.4, 0.0, 0.4);
+	glRotatef(90, 0.0, 1.0, 0.0);
+	glBegin(GL_QUADS);
+	glVertex3f(0.3f, 0.3f, 0.0);
+	glVertex3f(0.3f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.3f, 0.0);
+	glEnd();
+	glPopMatrix();
+
+	glPushMatrix();
+	glColor3f(0.74, 0.56, 0.56);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glTranslatef(0.3, 0.0, 0.4);
+	glRotatef(90, 0.0, 1.0, 0.0);
+	glBegin(GL_QUADS);
+	glVertex3f(0.3f, 0.3f, 0.0);
+	glVertex3f(0.3f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.5f, 0.0);
+	glVertex3f(0.4f, 0.3f, 0.0);
+	glEnd();
+	glPopMatrix();
+
+	glPushMatrix();
+	glColor3f(0.74, 0.56, 0.56);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glTranslatef(-0.2, 0.1, -0.2);
+	glRotatef(90, 1.0, 0.0, 0.0);
+	glBegin(GL_QUADS);
+	glVertex3f(0.7f, 0.3f, 0.0);
+	glVertex3f(0.7f, 0.2f, 0.0);
+	glVertex3f(0.4f, 0.2f, 0.0);
+	glVertex3f(0.4f, 0.3f, 0.0);
+	glEnd();
+	glPopMatrix();
+
+
+	glPushMatrix();
+	glColor3f(0.74, 0.56, 0.56);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glTranslatef(-0.3, 0.3, -0.2);
+	glRotatef(90, 1.0, 0.0, 0.0);
+	glBegin(GL_QUADS);
+	glVertex3f(0.9f, 0.3f, 0.0);
+	glVertex3f(0.9f, 0.2f, 0.0);
+	glVertex3f(0.4f, 0.2f, 0.0);
+	glVertex3f(0.4f, 0.3f, 0.0);
+	glEnd();
+	glPopMatrix();
+
+	//fim tronco
+
+	//velas
+	glPushMatrix();
+	glColor3f(0.75, 1.0, 0.0);
+	glScalef(8, 8, 8);
+	glTranslatef(x, y, z);
+	glBegin(GL_TRIANGLES);
+	glVertex3f(0.4f, 0.3f, 0.0);
+	glVertex3f(0.4f, 0.5f, 0.0);
+	glVertex3f(0.6f, 0.3f, 0.0);
+	glEnd();
+	glPopMatrix();
+
 }
 
 Inimigo::Inimigo()
