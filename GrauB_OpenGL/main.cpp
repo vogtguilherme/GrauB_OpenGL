fb18c1588f99331741c42e5ca0e1377ebd1ab638
Guilherme Vogt <vogt.guilherme@gmail.com>
2018-11-28 00:02:26 -0200

    Metodos para funções da opengl criados no main

--- GrauB_OpenGL/main.cpp
+++ GrauB_OpenGL/main.cpp
@@ -15,17 +15,15 @@ int main(int argc, char **argv)
 
 	glutCreateWindow("River Raid 3D");
 
-	//glutDisplayFunc(Desenha);
-	glutDisplayFunc(game.Render);
+	glutDisplayFunc(Desenha);
+	glutReshapeFunc(AlteraTamanhoJanela);
+	glutMouseFunc(GerenciaMouse);
+	glutKeyboardFunc(GerenciaTeclas);
 
-	//glutReshapeFunc(AlteraTamanhoJanela);
-	glutReshapeFunc(game.SetWindowSize);
-
-	//glutMouseFunc(GerenciaMouse);
-	glutMouseFunc(game.GetMouseInput);
-
-	//glutKeyboardFunc(GerenciaTeclas);
-	glutKeyboardFunc(game.GetKeyboardInput);
+	/*glutDisplayFunc(game.Render);	
+	glutReshapeFunc(game.SetWindowSize);	
+	glutMouseFunc(game.GetMouseInput);	
+	glutKeyboardFunc(game.GetKeyboardInput);*/
 
 	game.Start();
 
@@ -40,4 +38,72 @@ void runMainLoop(int val)
 	game.Update();
 
 	glutTimerFunc(1000 / game.FPS, runMainLoop, val);
+}
+
+void Desenha()
+{
+	// Limpa a janela e o depth buffer
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//glColor3f(0.0f, 0.0f, 1.0f);
+
+	// Desenha uma esfera sólida
+	//glutSolidSphere(50.0f, 100, 100);
+
+	Pecas.criaMar();
+	Pecas.CriaParedes();
+
+	/*
+	glRotatef(cubeAngle, cubeX, cubeY, cubeZ);
+	glTranslatef(moveX, moveY, moveZ);
+	glutSolidCube(50);
+	*/
+
+	for (int i = 0; i < 5; i++)
+	{
+		IA[i].DesenhaInimigo();
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		bullets[i].DesenhaBullet();
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		fuel[i].DesenhaFuel();
+
+	}
+
+	jogador.Desenhajogador();
+
+	/*EscreveVidas();
+	EscreveKills();
+	EscreveFuel();*/
+
+	glutSwapBuffers();
+}
+
+void AlteraTamanhoJanela(GLsizei w, GLsizei h)
+{
+	// Para previnir uma divisão por zero
+	if (h == 0) h = 1;
+
+	// Especifica o tamanho da viewport
+	glViewport(0, 0, w, h);
+
+	// Calcula a correção de aspecto
+	game.fAspect = (GLfloat)w / (GLfloat)h;
+
+	game.SpecifyViewParameters();
+}
+
+void GerenciaMouse(int button, int state, int x, int y)
+{
+
+}
+
+void GerenciaTeclas(unsigned char key, int x, int y)
+{
+
 }
